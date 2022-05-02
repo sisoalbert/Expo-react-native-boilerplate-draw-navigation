db527a16edd982eb96abcb56b8ad5e303e18db46
sisoalbert <sisoalbert@yahoo.com>
2022-05-02 14:09:18 +0200

    without redux

--- App.js
+++ App.js
@@ -4,19 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 
 import RootNavigator from "./src/navigation/RootNavigator";
 
-//Redux
-import { Provider } from "react-redux";
-import { store, appPersist } from "./src/redux/store";
-import { PersistGate } from "redux-persist/integration/react";
-
 const App = () => {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={appPersist}>
-        <RootNavigator />
-      </PersistGate>
-    </Provider>
-  );
+  return <RootNavigator />;
 };
 
 export default App;
